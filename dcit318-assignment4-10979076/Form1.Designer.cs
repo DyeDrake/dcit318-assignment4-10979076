f369aa224398d1e627c8f2db25c539137a7a9ad4
Clifford Anim Boateng <aboateng_clifford@st.ug.edu.gh>
2024-07-08 18:47:11 +0000

    Add project files.

---
+++ dcit318-assignment4-10979076/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace dcit318_assignment4_10979076
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ControlLightLight;
+            label1.Font = new Font("Segoe UI", 24F);
+            label1.ForeColor = SystemColors.ActiveCaption;
+            label1.Location = new Point(127, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(493, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome! Enter your Credentials ";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.GradientInactiveCaption;
+            textBox1.Location = new Point(285, 164);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(248, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F);
+            label2.ForeColor = SystemColors.AppWorkspace;
+            label2.Location = new Point(117, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(121, 32);
+            label2.TabIndex = 2;
+            label2.Text = "Username";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.GradientInactiveCaption;
+            textBox2.Location = new Point(285, 229);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(248, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F);
+            label3.ForeColor = SystemColors.AppWorkspace;
+            label3.Location = new Point(127, 220);
+            label3.Name = "label3";
+            label3.Size = new Size(111, 32);
+            label3.TabIndex = 4;
+            label3.Text = "Password";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Control;
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.Location = new Point(330, 286);
+            button1.Name = "button1";
+            button1.Size = new Size(144, 48);
+            button1.TabIndex = 5;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Button button1;
+    }
+}
